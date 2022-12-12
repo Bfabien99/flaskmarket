545f7cbbbb7cc51de3cb3e4981e9df7c1250be0a
bfabien99 <fabienbrou99@gmail.com>
2022-12-12 15:52:34 +0000

    redirect unauthorized user

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager= LoginManager(app)
+login_manager.login_view = "login_page"
 
 @login_manager.user_loader
 def load_user(user_id):
